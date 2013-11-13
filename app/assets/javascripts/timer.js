be134f0adf432c6939da71eba6c1d7e14689a23f
Scott Luptowski <scottluptowski@gmail.com>
2013-11-13 09:56:55 -0500

    remove comments

--- app/assets/javascripts/timer.js
+++ app/assets/javascripts/timer.js
@@ -4,8 +4,6 @@ function formatTime(num) {
     var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
     var seconds = sec_num - (hours * 3600) - (minutes * 60);
 
-    //if (hours   < 10) {hours   = "0"+hours;}
-    //if (minutes < 10) {minutes = "0"+minutes;}
     if (seconds < 10) {seconds = "0"+seconds;}
     var time    = minutes+':'+seconds;
     return time;
@@ -50,7 +48,6 @@ $(document).ready(function() {
         }
     }
     
-   
     $('#player1time').bind('click', playerOneClicked);
     $('#player2time').bind('click', playerTwoClicked);
  
