3889b3da984a1158f224a392798f8ca65e7d8087
Scott Luptowski <scottluptowski@gmail.com>
2013-11-13 11:30:34 -0500

    better formatTime

--- app/assets/javascripts/timer.js
+++ app/assets/javascripts/timer.js
@@ -1,8 +1,7 @@
 function formatTime(num) {
     var sec_num = parseInt(num, 10); 
-    var hours   = Math.floor(sec_num / 3600);
-    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
-    var seconds = sec_num - (hours * 3600) - (minutes * 60);
+    var minutes = Math.floor(sec_num / 60);
+    var seconds = sec_num - (minutes * 60);
 
     if (seconds < 10) {seconds = "0"+seconds;}
     var time    = minutes+':'+seconds;
